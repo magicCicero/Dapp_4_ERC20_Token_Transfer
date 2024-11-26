ece21601c93d493296a0ca049d0285bf845fd0b7
magicCicero <cicerooantonio20@gmail.com>
2024-11-26 10:29:30 -0300

    config to use mui

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// Create a custom theme (optional)
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#1976d2', // Blue color
+      },
+      secondary: {
+        main: '#dc004e', // Pink color
+      },
+    },
+  });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+           <CssBaseline /> {/* Resets browser styling */}
+           <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
